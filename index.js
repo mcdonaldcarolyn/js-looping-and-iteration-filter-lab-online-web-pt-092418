339910cdc514ea9faaa13ee9b2e8d593509e3816
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-25 10:15:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.toUpperCase() === name.toUpperCase();
+  })
+};
+function fuzzyMatch(drivers, name){
+  return drivers.filter(function (driver) {
+    return driver.substring(0,2) === name.substring(0,2);
+  })
+  };
+function matchName(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.name === name;
+  })
+  };
